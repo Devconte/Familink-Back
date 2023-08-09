b261e5edf2e456b4987d0f00ef128b47e737a6af
Michael Dutheil <mike.dutheil@gmail.com>
2023-08-09 15:49:32 +0200

    Fix: Send Mail

--- app/helpers/confirmEmailTemplate.js
+++ app/helpers/confirmEmailTemplate.js
@@ -1,4 +1,4 @@
-const url = process.env.NODE_ENV === 'development' ? 'href=http://localhost:3000/api/user/verify/' : process.env.API_URL_MAIL;
+const url = `href=${process.env.API_URL_MAIL}/api/verify/`;
 
 const confirmEmailTemplate = (user, token) => (
   {
