64ad7dd5b4a24066eb824ffad8ac5b108a5f2d45
Michael Dutheil <mike.dutheil@gmail.com>
2023-08-09 13:50:28 +0200

    fix: deploy

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ const logger = require('./helpers/logger');
 const router = require('./routers');
 const { swaggerSpec } = require('./helpers/swagger');
 const uploadMiddleware = require('./helpers/upload');
-const path = require('path');
 
 const app = express();
 
@@ -19,7 +18,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(uploadMiddleware);
-app.use(express.static('front_familink/dist'));
 app.use(express.static('app/images'));
 
 app.use((request, _, next) => {
@@ -29,9 +27,5 @@ app.use((request, _, next) => {
 app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api', router);
- 
-app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/home/student/Familink-Back/front_familink/dist', 'index.html'));
-});
 
 module.exports = app;
