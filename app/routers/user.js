3155de4a2410734bb6fa3c721e9666408739c44d
Devconte <romain.lcnte@gmail.com>
2023-08-10 18:58:07 +0200

    fix: added auth on user routes

--- app/routers/user.js
+++ app/routers/user.js
@@ -2,18 +2,19 @@ const express = require('express');
 const controllerHandler = require('../controllers/helpers/controllerHandler');
 const UserController = require('../controllers/UserController');
 const authController = require('../controllers/authController');
+const auth = require('../controllers/auth/authMiddleware');
 
 const router = express.Router();
 
 // bind is used to pass the class method as a reference, not call it directly
 router.post('/signup', controllerHandler(UserController.create.bind(UserController)));
 router.post('/login', controllerHandler(authController.login));
-router.post('/refreshtoken', controllerHandler(authController.tokenRefresh));
+router.post('/refreshtoken', auth, controllerHandler(authController.tokenRefresh));
 router.patch('/resetpassword', controllerHandler(authController.resetPassword));
-router.get('/:id', controllerHandler(UserController.getOne.bind(UserController)));
-router.patch('/:id/update', controllerHandler(UserController.update.bind(UserController)));
-router.delete('/:id/delete', controllerHandler(UserController.delete.bind(UserController)));
-router.patch('/:userId/assignto/:familyId', controllerHandler(UserController.asign_user_to_family.bind(UserController)));
+router.get('/:id', auth, controllerHandler(UserController.getOne.bind(UserController)));
+router.patch('/:id/update', auth, controllerHandler(UserController.update.bind(UserController)));
+router.delete('/:id/delete', auth, controllerHandler(UserController.delete.bind(UserController)));
+router.patch('/:userId/assignto/:familyId', auth, controllerHandler(UserController.asign_user_to_family.bind(UserController)));
 router.get('/verify/:token', controllerHandler(UserController.verifyEmailUser.bind(UserController)));
 router.post('/sendresetpassword', controllerHandler(UserController.sendResetEmailPassword.bind(UserController)));
 
